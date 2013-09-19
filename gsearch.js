1925c65398d355f5dff2b3803b6570ee787b6639
Ryan Flood <rflood@cmu.edu>
2013-09-18 20:03:59 -0400

    basic name functionality added

--- gsearch.js
+++ gsearch.js
@@ -3,3 +3,38 @@ function searchGithubCode(input, callback) {
     callback(data.responseJSON);
   }});
 }
+var response;
+function writeResults(jsonDump){
+  var container = $(".content-container");
+  var responses = jsonDump["total_count"] > 30 ? 30 :  jsonDump["total_count"];
+  var row = $("<div>").addClass("row");
+  var rowIndex = 0;
+  for(var i=0;i<responses;i++){
+    response = jsonDump["items"][i]
+    if(rowIndex < 4){
+      var item = $("<div>").addClass("container span3");
+      rowIndex+=1;
+    } else {
+      container.append(row);
+      row = $("<div>").addClass("row");
+      rowIndex=0;
+      var item = $("<div>").addClass("container span3");
+    }
+    item.append($("<h4>").text(response["name"]));
+    item.append($("<p>").text(response["field"]));
+    
+    row.append(item);
+  }
+  container.append(row);
+  container.append(responses);
+}
+
+// set onclick to do the search
+$(function(){
+  $(".searchButton").on("click",function(){
+    var searchQuery = $(".searchBox").val();
+    if (! searchQuery) {stuff = $(".searchBox");throw "yo";}
+    $(".searchBox").val("");
+    var jsonDump = searchGithubCode(searchQuery, writeResults);    
+  }) ;
+});
