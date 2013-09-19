d15f082049d06bfe3f6d8cf3e938b89493007a7f
Ryan Flood <rflood@cmu.edu>
2013-09-18 23:06:46 -0400

    basic styling of reponses... also added fragment functionality

--- gsearch.js
+++ gsearch.js
@@ -1,32 +1,33 @@
 function searchGithubCode(input, callback) {
-  $.ajax({url: 'https://api.github.com/search/code?q=' + input + '&sort=indexed', headers: {'Accept':'application/vnd.github.preview'}, complete: function(data) {
+  $.ajax({url: 'https://api.github.com/search/code?q=' + input + '&sort=indexed', headers: {'Accept':'application/vnd.github.preview.text-match'}, complete: function(data) {
     callback(data.responseJSON);
   }});
 }
 var response;
 function writeResults(jsonDump){
+  response = jsonDump
   var container = $(".content-container");
   var responses = jsonDump["total_count"] > 30 ? 30 :  jsonDump["total_count"];
   var row = $("<div>").addClass("row");
   var rowIndex = 0;
   for(var i=0;i<responses;i++){
     response = jsonDump["items"][i]
-    if(rowIndex < 4){
-      var item = $("<div>").addClass("container span3");
+    var item = $("<div>").addClass("container span4");
+    if(rowIndex < 2){
       rowIndex+=1;
     } else {
       container.append(row);
       row = $("<div>").addClass("row");
       rowIndex=0;
-      var item = $("<div>").addClass("container span3");
     }
-    item.append($("<h4>").text(response["name"]));
-    item.append($("<p>").text(response["field"]));
+    item.append($("<h4>").append(
+      $("<a>").text(response.repository.name)).attr('href',response.repository.url)
+    );
+    item.append($("<p>").text("by "+response.repository.owner.login));
     
     row.append(item);
   }
   container.append(row);
-  container.append(responses);
 }
 
 // set onclick to do the search
